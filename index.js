375340f8044931a17a9bccdaf4f40ee6b5f9fd19
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-02-23 12:20:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // for checking the api is working or not
-console.log("Keys", process.env.PORT, process.env.OPENAI_API_KEY)
+console.log("Keys", process.env)
 
 
 const app = express();
