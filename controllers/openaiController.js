a4d370179bd0c698283a0ddfe7d4355fd4abf675
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-02-23 10:09:06 +0530

    Update openaiController.js

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+ OPENAI_API_KEY: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
